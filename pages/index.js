996a70a45c27073ff8f7e840a170643b31f25d9d
Jayesh Shadi <44752916+jayesh-shadi@users.noreply.github.com>
2021-10-16 22:43:55 +0530

    initial

---
+++ pages/index.js
@@ -0,0 +1,66 @@
+import dynamic from "next/dynamic";
+import PropTypes from "prop-types";
+const Navigation = dynamic(() => import("../components/Navigation"));
+const Greetings = dynamic(() => import("../containers/Greetings"));
+const Skills = dynamic(() => import("../containers/Skills"));
+const Proficiency = dynamic(() => import("../containers/Proficiency"));
+const Education = dynamic(() => import("../containers/Education"));
+const Experience = dynamic(() => import("../containers/Experience"));
+const Projects = dynamic(() => import("../containers/Projects"));
+const GithubProfileCard = dynamic(() =>
+	import("../components/GithubProfileCard")
+);
+import { openSource } from "../portfolio";
+import SEO from "../components/SEO";
+
+export default function Home({ githubProfileData }) {
+	return (
+		<div>
+			<SEO
+				data={{
+					title: "JS|Portfolio",
+					description:
+						"A passionate Full Stack Web Developer.",
+					image:"https://avatars.githubusercontent.com/u/44752916?s=400&u=84fc8669cdf0afcb02a012f3ff0a3e7055b39b58&v=4",
+					url: "https://developer-portfolio.herokuapp.com/",
+					keywords: [
+						"full stack web developer",
+						"mobile app developer",
+						"android developer",
+						"django",
+						"flask",
+						"django rest framework",
+						"nodejs ",
+						"expressjs",
+						"reactjs ",
+						"contextapi",
+						"redux",
+						"flutter",
+					],
+				}}
+			/>
+			<Navigation />
+			<Greetings />
+			<Skills />
+			<Proficiency />
+			<Education />
+			<Experience />
+			<Projects />
+			<GithubProfileCard prof={githubProfileData} />
+		</div>
+	);
+}
+
+Home.prototype = {
+	githubProfileData: PropTypes.object.isRequired,
+};
+
+export async function getStaticProps(_) {
+	const githubProfileData = await fetch(
+		`https://api.github.com/users/${openSource.githubUserName}`
+	).then((res) => res.json());
+
+	return {
+		props: { githubProfileData },
+	};
+}
